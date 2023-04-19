dd6820e2c4526fa2fee3d9f302859f183c529897
CameronJWalker <cameron.jr.walker@icloud.com>
2023-04-18 19:09:08 -0700

    imported head and added title

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Layout from '../components/layout';
+import Head from 'next/head' 
 
 export default function Home() {
   return (
     <Layout>
+      <Head> 
+        <title> Home </title>
+      </Head>
       <div>main</div>
+
     </Layout>
   )
 }
