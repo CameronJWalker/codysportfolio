3887609927fe2a25f9d66b9b52f8f5786dd22c8b
CameronJWalker <cameron.jr.walker@icloud.com>
2023-04-22 11:21:14 -0700

    navbar styling, reordered some of the navbar

--- components/navbar.js
+++ components/navbar.js
@@ -3,16 +3,18 @@ import Nav from "../styles/Nav.module.css"
 
 export default function Navbar() {
     return (
-        <main className={Nav.container}>
-            <Link href="/" className={Nav.name} > 
-                CODY MEYER PHOTOGRAPHY
-            </Link>
-            <Link href="/gallery" className={Nav.gallery}>
-                Gallery 
-            </Link>
-            <Link href="/contact" className={Nav.contact}>
-                 Contact 
-            </Link>
+        <main>
+            <div className={Nav.container}>
+                <Link href="/" className={Nav.name} > 
+                    CODY MEYER PHOTOGRAPHY
+                </Link>
+                <div className={Nav.links}>
+                    <ul>
+                        <li><Link href="/gallery" className={Nav.gallery}>Gallery</Link></li>
+                        <li><Link href="/contact" className={Nav.contact}>Contact</Link></li>
+                    </ul>
+                </div>
+            </div>
         </main>
     )
 }
