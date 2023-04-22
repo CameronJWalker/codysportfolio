943ec8d6e95b76651f646a4adeccac16573ce693
CameronJWalker <cameron.jr.walker@icloud.com>
2023-04-22 16:36:55 -0700

    added drop down menu for navbar responsiveness

--- components/navbar.js
+++ components/navbar.js
@@ -2,12 +2,19 @@ import Link from "next/link"
 import Nav from "../styles/Nav.module.css"
 
 export default function Navbar() {
+    
+    
     return (
         <main>
             <div className={Nav.container}>
                 <Link href="/" className={Nav.name} > 
                     CODY MEYER PHOTOGRAPHY
                 </Link>
+                <a href="" className={Nav.toggleButton}>
+                    <span className={Nav.bar}></span>
+                    <span className={Nav.bar}></span>
+                    <span className={Nav.bar}></span>
+                </a>
                 <div className={Nav.links}>
                     <ul>
                         <li><Link href="/" className={Nav.home}>Home</Link></li>
