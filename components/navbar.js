d0b9bd6f1637b7296ee0cca8526806df5f1a6bd1
CameronJWalker <cameron.jr.walker@icloud.com>
2023-04-23 00:52:47 -0700

    redid some code for less lines, and some styling

--- components/navbar.js
+++ components/navbar.js
@@ -1,17 +1,13 @@
 import Link from "next/link"
 import Nav from "../styles/Nav.module.css"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 export default function Navbar() {
-    const [display, setDisplay] = useState(true);
-    const toggleButtonStyle = {
-        display: display ? "flex" : "none"
-    }
+    const [display, setDisplay] = useState(true)
 
     const handleClick = () => {
-        setDisplay(prevDisplay => !prevDisplay);
-        console.log(display)
-    };
+        setDisplay(!display)
+    }
 
     return (
         <main>
@@ -25,15 +21,15 @@ export default function Navbar() {
                     <span className={Nav.bar}></span>
                 </div>
                 { 
-                    display ? 
-                    <div className={Nav.links} style={toggleButtonStyle}>
+                    display && 
+                    
+                    <div className={Nav.links} >
                         <ul>
                             <li><Link href="/" className={Nav.home}>Home</Link></li>
                             <li><Link href="/gallery" className={Nav.gallery}>Gallery</Link></li>
                             <li><Link href="/contact" className={Nav.contact}>Contact</Link></li>
                         </ul>
                     </div> 
-                    : false
                 }
             </div>
         </main>
