61f53eb34f9e3e1d7d8db64dee7e4b87d87401c6
CameronJWalker <cameron.jr.walker@icloud.com>
2023-04-22 23:06:58 -0700

    added functionality for drop down menu, now closes and opens on click

--- components/navbar.js
+++ components/navbar.js
@@ -1,27 +1,47 @@
 import Link from "next/link"
 import Nav from "../styles/Nav.module.css"
+import { useState, useEffect } from "react"
 
 export default function Navbar() {
     
+    // const [display, setDisplay] = useState({})
     
+    // const handleClick = () => {
+    //     setDisplay({display: "flex"})
+    //     console.log(display)
+    // }
+    const [display, setDisplay] = useState(true);
+    const toggleButtonStyle = {
+        display: display ? "flex" : "none"
+    }
+
+    const handleClick = () => {
+        setDisplay(prevDisplay => !prevDisplay);
+        console.log(display)
+    };
+
     return (
         <main>
             <div className={Nav.container}>
                 <Link href="/" className={Nav.name} > 
                     CODY MEYER PHOTOGRAPHY
                 </Link>
-                <a href="" className={Nav.toggleButton}>
+                <div className={Nav.toggleButton} onClick={handleClick}>
                     <span className={Nav.bar}></span>
                     <span className={Nav.bar}></span>
                     <span className={Nav.bar}></span>
-                </a>
-                <div className={Nav.links}>
-                    <ul>
-                        <li><Link href="/" className={Nav.home}>Home</Link></li>
-                        <li><Link href="/gallery" className={Nav.gallery}>Gallery</Link></li>
-                        <li><Link href="/contact" className={Nav.contact}>Contact</Link></li>
-                    </ul>
                 </div>
+                { 
+                    display ? 
+                    <div className={Nav.links} style={toggleButtonStyle}>
+                        <ul>
+                            <li><Link href="/" className={Nav.home}>Home</Link></li>
+                            <li><Link href="/gallery" className={Nav.gallery}>Gallery</Link></li>
+                            <li><Link href="/contact" className={Nav.contact}>Contact</Link></li>
+                        </ul>
+                    </div> 
+                    : false
+                }
             </div>
         </main>
     )
