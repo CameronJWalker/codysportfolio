7f4d6952c8a098e618033738f285e919c99d85be
CameronJWalker <cameron.jr.walker@icloud.com>
2023-04-23 19:51:22 -0700

    added responsiveness to hamburger menu and navbar links on smaller screen sizes

--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,14 @@
 import Link from "next/link"
 import Nav from "../styles/Nav.module.css"
-import { useState, useEffect, useRef } from "react"
+import { useState, useRef } from "react"
 
 export default function Navbar() {
     const [display, setDisplay] = useState(true)
 
+    const myNavRef = useRef(null);
+
     const handleClick = () => {
+        myNavRef.current.style.display = display ? "flex" : "none";
         setDisplay(!display)
     }
 
@@ -20,17 +23,13 @@ export default function Navbar() {
                     <span className={Nav.bar}></span>
                     <span className={Nav.bar}></span>
                 </div>
-                { 
-                    display && 
-                    
-                    <div className={Nav.links} >
+                    <div className={Nav.links} ref={myNavRef}>
                         <ul>
                             <li><Link href="/" className={Nav.home}>Home</Link></li>
                             <li><Link href="/gallery" className={Nav.gallery}>Gallery</Link></li>
                             <li><Link href="/contact" className={Nav.contact}>Contact</Link></li>
                         </ul>
                     </div> 
-                }
             </div>
         </main>
     )
