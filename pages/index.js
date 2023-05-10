b9f369b12ad2f8064ceefb7637ba5d13478055dd
CameronJWalker <cameron.jr.walker@icloud.com>
2023-05-09 20:53:07 -0700

    added json with image url, name, and alt

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head' 
 import PhotoGrid from '../components/photoGrid'
 import Photo from '../styles/Photo.module.css'
+import images from '../images.json'
 
 export default function Home() {
 return (
@@ -9,18 +10,18 @@ return (
       <title> Home </title>
     </Head>
     <div className={Photo.grid}>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/CityVibes?t=2023-05-09T02%3A48%3A30.087Z"/>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/DesertRoad?t=2023-05-09T03%3A08%3A41.196Z"/>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/Photographer?t=2023-05-09T03%3A15%3A32.259Z"/>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/Isolated.jpg?t=2023-05-09T03%3A13%3A54.110Z"/>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/Eclipse.jpg?t=2023-05-09T03%3A13%3A45.793Z"/>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/OceanBubbles?t=2023-05-09T03%3A14%3A30.833Z"/>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/Self.jpg?t=2023-05-09T03%3A14%3A23.289Z"/>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/NaturesEyes?t=2023-05-09T03%3A14%3A02.971Z"/>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/Smoke?t=2023-05-09T03%3A14%3A17.502Z"/>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/Needle?t=2023-05-09T03%3A14%3A38.546Z"/>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/Surfer?t=2023-05-09T03%3A14%3A11.190Z"/>
-      <PhotoGrid imageUrl="https://daiiuznptrjzvlsexpwp.supabase.co/storage/v1/object/public/images/images/homepage/WakeUp.jpg?t=2023-05-09T03%3A15%3A08.700Z"/>
+      <PhotoGrid imageUrl={images.CityVibes.url} alt={images.CityVibes.alt}/>
+      <PhotoGrid imageUrl={images.DesertRoad.url} alt={images.DesertRoad.alt}/>
+      <PhotoGrid imageUrl={images.Photographer.url} alt={images.Photographer.alt}/>
+      <PhotoGrid imageUrl={images.Isolated.url} alt={images.Isolated.alt}/>
+      <PhotoGrid imageUrl={images.Eclipse.url} alt={images.Eclipse.alt}/>
+      <PhotoGrid imageUrl={images.OceanBubbles.url} alt={images.OceanBubbles.alt}/>
+      <PhotoGrid imageUrl={images.Self.url} alt={images.Self.alt}/>
+      <PhotoGrid imageUrl={images.NaturesEyes.url} alt={images.NaturesEyes.alt}/>
+      <PhotoGrid imageUrl={images.Smoke.url} alt={images.Smoke.alt}/>
+      <PhotoGrid imageUrl={images.Needle.url} alt={images.Needle.alt}/>
+      <PhotoGrid imageUrl={images.Surfer.url} alt={images.Surfer.alt}/>
+      <PhotoGrid imageUrl={images.WakeUp.url} alt={images.WakeUp.alt}/>
     </div>
   </div>
 )
