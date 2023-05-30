d08567366496c6c7497296db2a3ab76c53f9381b
Dylan <BooshBear03@gmail.com>
2023-05-30 10:38:46 -0700

    Added dark theme and fixed the instagram part

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from "../components/layout"
 import '../styles/globals.css'
 import Head from 'next/head'
 
+
 export default function App({ Component, pageProps }) {
   return(
     <Layout>
