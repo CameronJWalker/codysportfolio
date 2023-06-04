a7453e07026affc358188e96ad1e7729cc7fb3b6
CameronJWalker <cameron.jr.walker@icloud.com>
2023-06-03 19:48:20 -0700

    testing changes

--- components/photoGrid.js
+++ components/photoGrid.js
@@ -1,12 +1,11 @@
 import { React } from 'react';
-import Photo from '../styles/Photo.module.css'
-
-
+import Photo from '../styles/Photo.module.css';
+import Image from 'next/image';
 
 export default function PhotoGrid(props) {
 
     return(
-            <div>
+            <div className={Photo.container}>
                 <img 
                     src={props.imageUrl}
                     className={Photo.box}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
+import { React, useState, useEffect } from 'react';
 import Head from 'next/head' 
 import PhotoGrid from '../components/photoGrid'
 import Photo from '../styles/Photo.module.css'
-import images from '../images/sqimages.json'
+import imagesArray from '../images/sqimages.json'
 
 export default function Home() {
+
 return (
   <div className={Photo.container}>
     <Head> 
       <title> Home </title>
     </Head>
     <div className={Photo.grid}>
-      {images.map((image, index) => (
-          <PhotoGrid key={index} imageUrl={image.url} />
+      {imagesArray.map((image, index) => (
+          <PhotoGrid key={index} imageUrl={image.url} alt={image.alt}/>
         ))}
     </div>
   </div>
